ad438d1f40e04c6610b3103cece6f85c416f1e8d
Bruno La Plena <brunobtbam@gmail.com>
2026-04-21 14:22:01 +0200

    Created FavoritesPanel

---
+++ src/app/components/favorites-panel/favorites-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favorites-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './favorites-panel.component.html',
+  styleUrl: './favorites-panel.component.scss'
+})
+export class FavoritesPanelComponent {
+
+}
